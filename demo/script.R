76c98c20349b8acc66794e5bded4e6f10ec4e8c4
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-10-16 12:36:15 +0100

    Reduce script for MNIST data right down

--- demo/script.R
+++ demo/script.R
@@ -1,110 +1,23 @@
 library(magrittr)
 library(cerebrum)
 
+
 # First step is to load up the image datasets
 data.set <- cerebrum::load_mnist()
 
-# Convert labels to matrix
-allLabs <- data.set$labels %>%
-  unique %>%
-  sort
-
-# Create empty output matrices
-empt <- matrix(0 %>% rep(allLabs %>% length))
-
-# Update the actual labels
-newLabels <- lapply(
-  X = data.set$labels,
-  FUN = function(x) {
-    current <- empt
-    current[x + 1, ] <- 1
-    current
-  }
-)
-
-# Generate random samples
-reorder <- data.set$labels %>%
-  length %>%
-  sample
-
-# Now re-order data AND labels
-newLabels %<>% `[`(reorder)
-data.set$dat <- data.set$dat[reorder, ] / 256
-
-# Split up training + testing
-train.data <- data.set$dat[1:50000, ]
-train.labels <- newLabels[1:50000]
-test.data <- data.set$dat[50001:60000, ]
-test.labels <- newLabels[50001:60000]
+# Get the right structure for the data
+my.data <- data.set %>%
+  cerebrum::update_data()
 
 # Initialise a neural network with a single hidden layer (with 5 neurons)!
-res <- c(data.set$dat %>% ncol, 30, allLabs %>% length) %>%
+res <- c(my.data$train %>% ncol, 30, my.data$labels.tr[[1]] %>% nrow) %>%
   cerebrum::nnetwork()
 
-# Define standard variables...
-batchsize <- 10
-epochs <- 10
-eta <- 3.0
+# Test data
+res %>% cerebrum::optimize_and_test(
+  train.data = my.data$train,
+  train.labels = my.data$labels.tr,
+  test.data = my.data$test,
+  test.labels = my.data$labels.te
+)
 
-# Optimize weights + biases
-for (i in 1:epochs) {
-
-  # Sample the training data
-  splitup <- train.data %>%
-    nrow %>%
-    sample
-
-  batchContrib <- 1:(train.data %>% nrow %>% `/`(batchsize))
-
-  new.train <- lapply(
-    X = batchContrib,
-    FUN = function(x) {
-      train.data[(((x - 1) * batchsize) + 1):(x * batchsize), ]
-    }
-  )
-
-  new.labels <- lapply(
-    X = batchContrib,
-    FUN = function(x) {
-      train.labels[(((x - 1) * batchsize) + 1):(x * batchsize)]
-    }
-  )
-
-  # Set up progress bar
-  pb <- utils::txtProgressBar(
-    min = 0,
-    max = train.data %>% nrow %>% `/`(batchsize),
-    style = 3
-  )
-
-  # Gradient descent for all batches
-  for (k in 1:(train.data %>% nrow %>% `/`(batchsize))) {
-    res %<>% cerebrum::s_grad_desc(
-      xs = new.train[[k]],
-      ys = new.labels[[k]],
-      eta = eta
-    )
-    utils::setTxtProgressBar(pb, value = k)
-  }
-
-  # Now check out the test data (it doesnt need shuffled!)
-  totsum <- 0
-  for (k in 1:(test.data %>% nrow)) {
-
-  # Calculate the highest feed forward activation
-  my_res <- test.data[k, ] %>%
-    cerebrum::feed_forward(
-      res = res
-    ) %>%
-    `[[`("activations") %>%
-    `[[`(3) %>%
-    as.double %>%
-    which.max
-
-   # If it matches then increment the count!
-   if (test.labels[[k]][my_res, 1] == 1) totsum %<>% `+`(1)
-  }
-
-  cat(paste0("Epoch ", i, " : ", totsum / (test.data %>% nrow)), "\n")
-
-}

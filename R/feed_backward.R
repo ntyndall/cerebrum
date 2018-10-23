9cc1963b08e78e54ce7397cd4b5f58442820a40e
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-10-23 14:29:55 +0100

    Clean up backwards feed functionality

--- R/feed_backward.R
+++ R/feed_backward.R
@@ -5,14 +5,17 @@
 
 feed_backward <- function(forward, res, outputs = matrix(1)) {
 
+  # Function for retrieving the last element of a list
+  last_element <- function(x) x %>% utils::tail(1) %>% `[[`(1)
+
   # Calculate delta in the final layer first
   # i.e. del C . sigma'
   nablaC <- forward$activations %>%
-    `[[`(forward$activations %>% length) %>%
+    last_element() %>%
     `-`(outputs)
 
   sigPrime <- forward$wInputs %>%
-    `[[`(forward$wInputs %>% length) %>%
+    last_element() %>%
     cerebrum::sig(prime = T)
 
   deltaL <- nablaC %>%
@@ -22,18 +25,39 @@ feed_backward <- function(forward, res, outputs = matrix(1)) {
   nablaW <- res$weights %>% cerebrum::zero_mat()
   nablaB <- res$bias %>% cerebrum::zero_mat()
 
+  # Useful function for calculating
+  nab_w <- function(del, act, val) del %*% (act %>% `[[`(val) %>% t)
+
   # Put in the very last nablas
   nablaB[[nablaB %>% length]] <- deltaL
-  nablaW[[nablaW %>% length]] <- deltaL %*% (forward$activations[[forward$activations %>% length %>% `-`(1)]] %>% t)
+  nablaW[[nablaW %>% length]] <- nab_w(
+    del = deltaL,
+    act = forward$activations,
+    val = forward$activations %>% length %>% `-`(1)
+  )
 
   # Loop over the remaining layers.. (backwards)!
   # We know that the errors are related by adjacent layers
   for (i in (res$layers - 2):1) {
-    zval <- forward$wInputs[[i]]
-    sprime <- zval %>% cerebrum::sig(prime = T)
-    nudel <- res$weights[[i + 1]] %>% t %*% deltaL %>% matrixcalc::hadamard.prod(sprime)
+    # Calculate sigma prime
+    sprime <- forward$wInputs[[i]] %>%
+      sig(prime = T)
+
+    # Calculate the new delta value
+    nudel <- exampleNN$weights[[i + 1]] %>%
+      t %*%
+      deltaL %>%
+      matrixcalc::hadamard.prod(sprime)
+
+    # Update the b and w tilde values
     nablaB[[i]] <- nudel
-    nablaW[[i]] <- nudel %*% (forward$activations[[i]] %>% t)
+    nablaW[[i]] <- nab_w(
+      del = nudel,
+      act = forward$activations,
+      val = i
+    )
+
+    # Use this delta in the next iteration
     deltaL <- nudel
   }
 
